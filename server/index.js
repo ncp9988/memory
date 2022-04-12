0e53b8f6d457a566db6f0adcae36019e04bab1a1
Shelby Pham <ncp9988@gmail.com>
2022-04-12 16:37:09 -0500

    connected to server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser  from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECT_URL = 'mongodb+srv://ncp9988:12345@cluster-memory.vxajz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
+//https://www.mongodb.com/cloud/atlas
