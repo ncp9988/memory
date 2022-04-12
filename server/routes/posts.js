13174b623696a00979e88ef46bec2396261fc989
Shelby Pham <ncp9988@gmail.com>
2022-04-12 16:51:13 -0500

    connected to server

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+// localhost:5000/posts
+
+router.get('/', (req, res) => {
+    res.send('THIS WORKS!');
+});
+
+export default router;
+
+
