540032e16325fa52627e42992ef70f57c4aa1f48
Shelby Pham <ncp9988@gmail.com>
2022-04-15 10:27:27 -0500

    components structured

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 
-import memories from './images/memo.jpeg';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+
+import memories from './images/memory.jpeg';
 const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar position='static' color='inherit'>
                 <Typography variant='h2' align='center'>Memories</Typography>
-                <img src={memories} alt="memories" height="60"/>
+                <img src={memories} alt="memories" height="500" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
